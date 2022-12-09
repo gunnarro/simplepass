1df07570b1dad85b118da521a64c863e910c7df0
Gunnar Ronneberg <gunnar.ronneberg@gmail.com>
2022-12-09 04:49:45 +0100

    Update SimpleCredStoreApplicationException.java
    code smell fixes

--- app/src/main/java/com/gunnarro/android/simplepass/exception/SimpleCredStoreApplicationException.java
+++ app/src/main/java/com/gunnarro/android/simplepass/exception/SimpleCredStoreApplicationException.java
@@ -1,8 +1,8 @@
 package com.gunnarro.android.simplepass.exception;
 
 public class SimpleCredStoreApplicationException extends RuntimeException {
-    private final static String DEFAULT_MESSAGE_FORMAT = "Application error! Please Report error to app developer. Error=%s";
-    String errorCode;
+    private static final String DEFAULT_MESSAGE_FORMAT = "Application error! Please Report error to app developer. Error=%s";
+    private String final errorCode;
 
     public SimpleCredStoreApplicationException(String msg, String errorCode, Throwable throwable) {
         super(String.format(String.format(DEFAULT_MESSAGE_FORMAT, msg), errorCode), throwable);
