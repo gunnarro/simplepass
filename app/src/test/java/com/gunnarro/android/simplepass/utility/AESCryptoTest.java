315b56d982c44791478c963848f5b08c4b5b25f3
gunnarro <gunnar_ronneberg@yahoo.no>
2022-12-11 19:37:37 +0100

    fix

--- app/src/test/java/com/gunnarro/android/simplepass/utility/AESCryptoTest.java
+++ app/src/test/java/com/gunnarro/android/simplepass/utility/AESCryptoTest.java
@@ -1,8 +1,8 @@
 package com.gunnarro.android.simplepass.utility;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
